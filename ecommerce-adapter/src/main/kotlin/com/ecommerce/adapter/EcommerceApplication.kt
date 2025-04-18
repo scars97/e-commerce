43278d4b3861863e6aec3fbe89ed3d2d016729e1
박성현 <qkr970504@gmail.com>
2025-04-18 22:42:35 +0900

    [chore] 멀티 모듈 설정
    - Mock API 를 위한 adapter 모듈 추가

--- ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/EcommerceApplication.kt
+++ ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/EcommerceApplication.kt
@@ -1,4 +1,4 @@
-package com.study.ecommerce
+package com.ecommerce.adapter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "ecommerce"
+
+include("ecommerce-adapter")
