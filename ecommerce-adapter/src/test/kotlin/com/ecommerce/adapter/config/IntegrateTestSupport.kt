8cc15bb62c014b6ac9d41b9d158ec98ceccc10d1
박성현 <qkr970504@gmail.com>
2025-04-22 17:39:54 +0900

    [chore] Test Container 설정

---
+++ ecommerce-adapter/src/test/kotlin/com/ecommerce/adapter/config/IntegrateTestSupport.kt
@@ -0,0 +1,20 @@
+package com.ecommerce.adapter.config
+
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IntegrateTestSupport {
+
+    @Autowired
+    private lateinit var databaseCleanUp: DatabaseCleanUp
+
+    @BeforeEach
+    fun setUp() {
+        databaseCleanUp.execute()
+    }
+
+}
