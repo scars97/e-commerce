a7a77ae9fc087d7a4dc6f10ebc70bff592c30984
박성현 <qkr970504@gmail.com>
2025-05-01 22:44:21 +0900

    [feat] 결제 controller 구현

--- ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/in/dto/response/PaymentResponse.kt
+++ ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/in/dto/response/PaymentResponse.kt
@@ -1,5 +1,6 @@
 package com.ecommerce.adapter.`in`.dto.response
 
+import com.ecommerce.domain.Payment
 import java.math.BigDecimal
 
 data class PaymentResponse(
@@ -8,4 +9,18 @@ data class PaymentResponse(
     val userId: Long,
     val price: BigDecimal,
     val status: String
-)
+) {
+
+    companion object {
+        fun of(payment: Payment): PaymentResponse {
+            return PaymentResponse(
+                paymentId = payment.id,
+                orderId = payment.orderId,
+                userId = payment.userId,
+                price = payment.price,
+                status = payment.status.name
+            )
+        }
+    }
+
+}
