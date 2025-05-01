7fbf1763d3d884efa6da77638be18b288fb25c6c
박성현 <qkr970504@gmail.com>
2025-05-01 22:43:24 +0900

    [feat] 상품 조회 controller 구현

--- ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/in/dto/response/PopularItemResponse.kt
+++ ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/in/dto/response/PopularItemResponse.kt
@@ -1,7 +1,21 @@
 package com.ecommerce.adapter.`in`.dto.response
 
+import com.ecommerce.domain.item.PopularItem
+
 data class PopularItemResponse(
     val rank: Int,
     val cumulativeSales: Long,
     val item: ItemResponse
-)
+) {
+
+    companion object {
+        fun of(popularItem: PopularItem): PopularItemResponse {
+            return PopularItemResponse(
+                rank = popularItem.rank,
+                cumulativeSales = popularItem.cumulativeSales,
+                item = ItemResponse.of(popularItem.item)
+            )
+        }
+    }
+
+}
