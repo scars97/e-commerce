0acb3ff165d4f6f5115aeb2a9eb05db994d9a6c1
박성현 <qkr970504@gmail.com>
2025-06-13 17:21:17 +0900

    [refactor] 인기 상품 조회 리팩토링
    - 세부 구현 -> 메서드 추상화

--- ecommerce-application/src/main/kotlin/com/ecommerce/application/service/ItemService.kt
+++ ecommerce-application/src/main/kotlin/com/ecommerce/application/service/ItemService.kt
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class ItemService(
     private val itemPort: ItemPort,
     private val orderItemPort: OrderItemPort
@@ -20,25 +21,16 @@ class ItemService(
         const val ITEM_LIMIT = 10
     }
 
-    @Transactional(readOnly = true)
     override fun getItems(page: Int, size: Int): Page<Item> {
         return itemPort.getItemsByPage(page, size)
     }
 
-    @Transactional(readOnly = true)
     override fun getPopularItemsOnTop10(period: Long): List<PopularItem> {
         // period 만큼 주문 상품 전체 조회
         val orderItemsByPeriod = orderItemPort.getOrderItemsByPeriod(period)
 
         // 중복 제거 후, 주문 수량 합 계산
-        val orderItems = orderItemsByPeriod
-            .groupBy { it.itemId }
-            .map { (itemId, items) ->
-                val totalQuantity = items.sumOf { it.quantity }
-                OrderItem(null, itemId, totalQuantity)
-            }
-            .sortedByDescending { it.quantity }
-            .take(ITEM_LIMIT)
+        val orderItems = deduplicateAfterCalculateTotalQuantity(orderItemsByPeriod)
 
         // 정렬된 주문 상품의 id로 실제 상품 조회
         val items = itemPort.getItemsIn(
@@ -46,6 +38,13 @@ class ItemService(
         )
 
         // 인기 상품 변환
+        return convertPopularItems(items, orderItems)
+    }
+
+    private fun convertPopularItems(
+        items: List<Item>,
+        orderItems: List<OrderItem>
+    ): List<PopularItem> {
         val itemOfId = items.associateBy { it.id }
         return orderItems.mapIndexed { index, item ->
             PopularItem(
@@ -55,5 +54,15 @@ class ItemService(
             )
         }
     }
+
+    private fun deduplicateAfterCalculateTotalQuantity(orderItemsByPeriod: List<OrderItem>) =
+        orderItemsByPeriod
+            .groupBy { it.itemId }
+            .map { (itemId, items) ->
+                val totalQuantity = items.sumOf { it.quantity }
+                OrderItem(null, itemId, totalQuantity)
+            }
+            .sortedByDescending { it.quantity }
+            .take(ITEM_LIMIT)
 
 }
