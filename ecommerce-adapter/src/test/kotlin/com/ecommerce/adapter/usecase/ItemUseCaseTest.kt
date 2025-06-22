83ad70a60ed47adeebdf235ad323bfaab501d215
박성현 <qkr970504@gmail.com>
2025-06-22 15:45:09 +0900

    [refactor] 인기 상품 조회 방식 변경

--- ecommerce-adapter/src/test/kotlin/com/ecommerce/adapter/usecase/ItemUseCaseTest.kt
+++ ecommerce-adapter/src/test/kotlin/com/ecommerce/adapter/usecase/ItemUseCaseTest.kt
@@ -3,6 +3,7 @@ package com.ecommerce.adapter.usecase
 import com.ecommerce.adapter.config.IntegrateTestSupport
 import com.ecommerce.adapter.fixture.OrderFixture
 import com.ecommerce.application.port.`in`.ItemUseCase
+import com.ecommerce.application.schedule.ItemRankSchedule
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 
 class ItemUseCaseTest @Autowired constructor(
     private val sut: ItemUseCase,
+    private val rankScheduler: ItemRankSchedule,
     private val orderFixture: OrderFixture
 ): IntegrateTestSupport() {
 
@@ -19,22 +21,23 @@ class ItemUseCaseTest @Autowired constructor(
     }
 
     @Test
-    fun getPopularItemsOnTop10() {
+    fun getPopularItems() {
         // given
         val period = 3L
+        rankScheduler.aggregateOrderStatistics()
 
         // when
-        val popularItems = sut.getPopularItemsOnTop10(period)
+        val result = sut.getPopularItems(period)
 
         // then
-        assertThat(popularItems).hasSize(5)
-            .extracting("rank", "cumulativeSales", "item.id", "item.name")
+        assertThat(result).hasSize(5)
+            .extracting("rank", "item.id", "item.name")
             .containsExactly(
-                tuple(1, 20L, 5L, "상품 E"),
-                tuple(2, 18L, 2L, "상품 B"),
-                tuple(3, 15L, 1L, "상품 A"),
-                tuple(4, 12L, 4L, "상품 D"),
-                tuple(5, 10L, 3L, "상품 C")
+                tuple(1, 5L, "상품 E"),
+                tuple(2, 2L, "상품 B"),
+                tuple(3, 1L, "상품 A"),
+                tuple(4, 4L, "상품 D"),
+                tuple(5, 3L, "상품 C")
             )
     }
 
